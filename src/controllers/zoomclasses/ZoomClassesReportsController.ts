c814632975c7f85d218c18c8101a8fd6e66e2c5b
techzgigz <gigz.techz@gmail.com>
2021-10-26 02:04:43 +0530

    ZOOM

--- src/controllers/zoomclasses/ZoomClassesReportsController.ts
+++ src/controllers/zoomclasses/ZoomClassesReportsController.ts
@@ -21,12 +21,11 @@ import {
 
 } from "@tsed/schema";
 import { AcceptRoles } from "src/decorators/AcceptRoles";
-import { ZoomclassesReport } from "src/models/Zoomclasses/ZoomClassesReport";//
+import { ZoomClassesReport } from "src/models/Zoomclasses/ZoomClassesReport";//
 import { ZoomClassesReportService } from "src/services/ZoomClassesReportsService";//
 import { User } from "src/models/users/User";
 import { UsersService } from "src/services/UsersService";
-import * as jwt from "jsonwebtoken";// import * as multer from 'multer'
-import axios from 'request-promise';
+
 
 @Controller("/ZoomClassesReport")//
 export class ZoomClassesReportsController {
@@ -80,73 +79,15 @@ export class ZoomClassesReportsController {
   ): Promise<ZoomClassesReport> {
 
     const user = await this.usersService.find(data.createdBy.toString());
-    if (!user) {
+    if (!user)  role: (request.user as any).role,
+    _id: (request.user as any)._id,
+    adminId: (request.user as any).adminId,
+  });{
       throw new Error(
         `User with id: ${data.createdBy} doesn't exist or is superadmin, use other role.`
       );
     } 
-    const  YOUR_CLIENT_SECRET : any = process.env.YOUR_CLIENT_SECRET;
-    const now = Date.now(); 
-    const JWT= await jwt.sign(
-      {
-        iss: process.env.YOUR_CLIENT_ID, 
-        exp: now + 3600 * 1000
-      },
-      YOUR_CLIENT_SECRET
-    );
-
-    var options = { 
-      method: 'POST',
-      uri: 'https://api.zoom.us/v2/users/36t77U09T7GWMGpZAwh4JQ/meetings',
-      body: {
-        //status: 'active',
-        host_id: data.createdBy, topic: data.classtittle, type: data.type  ,"start_time": data.classdate,
-        "duration": data.duration,
-        //"schedule_for": data.createdBy,// -> uri + '?status=active'
-      },
-      auth: {
-        //Provide your token here
-        'bearer': JWT//'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJORnV1Nklrc1RjeUpxWElrMzR5YkFnIiwiZXhwIjoxNjMzNTIwNTUwNTg0LCJpYXQiOjE2MzM1MTY5NTB9.idgcj8DM82-gi8d1ZOpyW-J-KzL5Hf0UnpeNFq1LyAA'
-      },
-      json: true,
-      headers: {
-        'User-Agent': 'Zoom-Jwt-Request',
-        'content-type': 'application/json',
-
-      }// Automatically parses the JSON string in the response
-    };
-    let _this=this;
-    //
-    //try {
-     await axios(options).then(function (meeting: any) {
-      data.options = JSON.stringify(meeting);
-      //  return response.data;
-      //console.log(meeting)
-      return _this.ZoomClassesReportsService.save(data, {
-        role: (request.user as any).role,
-        _id: (request.user as any)._id,
-        adminId: (request.user as any).adminId,
-      });
-
-    })
-    .catch(function (err:any) {
-      // API call failed...
-      console.log('API call failed, reason ', err);
-      throw new Error(
-        `API call failed, reason `
-      );
-  });
-  // .catch (err) {
-  //    
-  }
-
-
-
-  @Put("/:id")
-  @Security("oauth_jwt")
-  @Authorize("jwt")
-  @AcceptRoles("admin")
-  @Summary("Update ZoomClassesReports with id")
+    
   @Status(201, { description: "Updated ZoomClassesReports", type:ZoomClassesReport })
   update(
     @PathParams("id") @Required() id: string,
